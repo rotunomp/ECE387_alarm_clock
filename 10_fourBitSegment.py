1502c423f5dfe619fda6fcbf14eb76cdcf685a2b
rotunomp <rotunomp@miamioh.edu>
2019-03-10 23:15:59 -0400

    added an alarm set function

--- 10_fourBitSegment.py
+++ 10_fourBitSegment.py
@@ -16,6 +16,7 @@ pins = [11,12,13,15,16,18,22,7,3,5,24,26]
 bits = [BIT0, BIT1, BIT2, BIT3]
 ALARM_PIN = 32
 BUTTON_PIN = 36
+CHANGE_PIN = 38
 
 ## alarm variable, set to 00:00:00 by default
 alarm_time = CLOCK(23, 50)
@@ -56,7 +57,58 @@ def change_clock_time():
                 minutes = minutes + 1
 
         clock_time = CLOCK(hours, minutes)
-        
+
+def change_alarm_time():
+
+        global alarm_time
+
+        minutes = alarm_time.minute
+        hours = alarm_time.hour
+
+        ## counts how many time units have passed since the last button press
+        ## so the alarm can be set, car-clock-style
+        delay_counter = 0
+
+        while delay_counter < 5:
+                if GPIO.input(CHANGE_PIN) == False: ## active low
+                        if minutes == 59:
+                                minutes = 0
+                                hours = hours + 1
+                                if hours == 24:
+                                        hours = 0
+                        else:
+                                minutes = minutes + 1
+                        delay_counter = 0
+                        
+                alarm_time = CLOCK(hours, minutes)
+                for i in range(50):
+                        display(alarm_time)
+                ## add a little flash to show we are setting the time
+                time.sleep(0.15)
+                delay_counter = delay_counter + 1
+
+        ## reset delay counter and sleep for a bit to show change to hour set
+        delay_counter = 0
+        time.sleep(2)
+
+        while delay_counter < 5:
+                if GPIO.input(CHANGE_PIN) == False: ## active low
+                        if hours == 23:
+                                hours = 0
+                        else:
+                                hours = hours + 1
+                        delay_counter = 0
+
+                alarm_time = CLOCK(hours, minutes)
+                for i in range(50):
+                        display(alarm_time)
+                ## add a little flash to show we are setting the time
+                time.sleep(0.15)
+                delay_counter = delay_counter + 1
+
+        return
+                
+
 
 ## handles displaying the time on the 7-segment
 ## each run of this method is ~8 milliseconds
@@ -114,8 +166,9 @@ def setup():
 	GPIO.setup(ALARM_PIN, GPIO.OUT)
 	GPIO.output(ALARM_PIN, GPIO.LOW)
 
-	## setup the push button
+	## setup the push buttons
 	GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(CHANGE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 ## loop that keeps the clock running
@@ -126,6 +179,10 @@ def loop():
 		change_clock_time()
 		check_alarm_on()
 		check_turn_alarm_off()
+		
+                if GPIO.input(CHANGE_PIN) == False: ## active low
+                        change_alarm_time()
+                        
 		for i in range(100):  
 			display(clock_time)
 
